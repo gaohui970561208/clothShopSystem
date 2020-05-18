59764d8aa2f151710053bdf060931c97b3739261
gaohui <hui.gao@ourwill.com.cn>
2020-05-18 20:37:54 +0800

    fix: udpate

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,12 +5,15 @@ import productRouter from "./product";
 import orderRouter from "./orders";
 import feedbackRouter from "./feedback";
 
+//这里的api是路径总成，代表着前端请求的地址中需要添加api这个字段，比如服务后端的地址为http://127.0.0.1:3000; 此时请求的地址需要是http://127.0.0.1:3000/api
 const router = express.Router();
 const app = express();
+//类型封装，下面不同的路径对应不同的作用的接口
 router.use('/users', usersRouter);
 router.use('/shop', shopRouter);
 router.use('/product', productRouter);
 router.use('/order', orderRouter);
 router.use('/feedback', feedbackRouter);
 
+//接口添加后需要抛出，否则app.js接受不到所需要的路径信息
 module.exports = router;
